7b6a0a1df22bd735d856a1259110808ebbf4b019
Pranav <pranavmunot09@gmail.com>
2020-11-26 21:31:39 +0530

    first_commit

---
+++ src/API/Apicall.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+function Apicall(props) {
+  let [isLoad, setLoad] = useState(true);
+  let [rate, setRate] = useState(0);
+
+  useEffect(() => {
+    axios
+      .get(props.url)
+      .then((response) => {
+        let objData = response.data.rates;
+        if (objData[props.to]) {
+          console.log("yes");
+
+          setRate(objData[props.to]);
+        } else {
+          console.log("NO");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  return (
+    <div>
+      <p>{props.name}</p>
+      <p>{props.url}</p>
+      <p>
+        {props.to}
+        {"  "}
+        {props.amount * rate}
+      </p>
+    </div>
+  );
+}
+
+export default Apicall;
--- src/App.js
+++ src/App.js
@@ -1,23 +1,122 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import {
+  Button,
+  Container,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  Card,
+  TextField,
+} from "@material-ui/core";
+import "./App.css";
+import Apicall from "./API/Apicall";
 
 function App() {
+  let [from, setFrom] = useState("from");
+  let [to, setTo] = useState("to");
+  let [amount, setAmount] = useState(0);
+  let [checkInfo, setCheckInfo] = useState(false);
+
+  let fromHandler = (e) => {
+    setFrom(e.target.value);
+  };
+  let toHandler = (e) => {
+    setTo(e.target.value);
+  };
+  let amountHandler = (e) => {
+    setAmount(e.target.value);
+  };
+
+  let ButtonClick = () => {
+    if (from !== "None" && to !== "None" && amount > 0) {
+      setCheckInfo(true);
+    } else {
+      setCheckInfo(false);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <h1>Forex Aggregator</h1>
+        <form>
+          <div className="queryBox">
+            <FormControl variant="filled" className="Selector">
+              <TextField
+                id="filled-basic"
+                value={amount}
+                onChange={amountHandler}
+                label="Amount"
+                variant="filled"
+              />
+            </FormControl>
+            <FormControl variant="filled" className="Selector">
+              <InputLabel id="fromLabel">From</InputLabel>
+              <Select
+                className=""
+                labelId="fromLabel"
+                value={from}
+                onChange={fromHandler}
+                label="Select"
+              >
+                <MenuItem value={""}>
+                  <em>None</em>
+                </MenuItem>
+                <MenuItem value={"USD"}>USD</MenuItem>
+                <MenuItem value={"EUR"}>EUR</MenuItem>
+                <MenuItem value={"INR"}>INR</MenuItem>
+              </Select>
+            </FormControl>
+            <FormControl variant="filled" className="Selector">
+              <InputLabel id="fromLabel">To</InputLabel>
+              <Select
+                className=""
+                labelId="fromLabel"
+                value={to}
+                onChange={toHandler}
+              >
+                <MenuItem value={""}>
+                  <em>None</em>
+                </MenuItem>
+                <MenuItem value={"USD"}>USD</MenuItem>
+                <MenuItem value={"EUR"}>EUR</MenuItem>
+                <MenuItem value={"INR"}>INR</MenuItem>
+              </Select>
+            </FormControl>
+            <Button
+              fullWidth
+              variant="contained"
+              className="button"
+              color="primary"
+              onClick={ButtonClick}
+            >
+              {" "}
+              Convert{" "}
+            </Button>
+          </div>
+        </form>
+        {checkInfo ? (
+          <div>
+            <Card>
+              <Apicall
+                name="Exchange Rate API"
+                url={"https://api.exchangeratesapi.io/latest?base=" + from}
+                amount={amount}
+                to={to}
+              />
+            </Card>
+            <Card>
+              <Apicall
+                name="Rates API"
+                url={"https://api.ratesapi.io/api/latest?base=" + from}
+                amount={amount}
+                to={to}
+              />
+            </Card>
+          </div>
+        ) : null}
+      </Container>
     </div>
   );
 }
